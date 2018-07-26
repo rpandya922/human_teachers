215ed8540aa55148798ae7109695a78b83283bde
Ravi Pandya <rpandya922@berkeley.edu>
2018-07-26 11:33:37 -0700

    functionality for featurizing grids etc

--- gridworld/utils.py
+++ gridworld/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
 from grid import GridWorld
+import math
 from environment import Environment
 
 def plot_cost_function(costs, grid, title=''):
@@ -156,3 +157,138 @@ def vi_optimal_trajectory(env, theta, style=None):
         path.append(loc)
         seen.add(loc)
     return path
+
+def ij_to_idx(i, j, n): return (i * n) + j
+def path_to_ij(m, n, path):
+    # path: list of indices in 2D array
+    # n, m: dimensions of array
+    ij_path = []
+    for idx in path:
+        i = math.floor(idx / m) + np.random.uniform(-0.1, 0.1)
+        j = idx % n + np.random.uniform(-0.1, 0.1)
+        ij_path.append([i, j])
+    return np.array(ij_path)
+def state_idx_to_ij(idx, grid):
+    """
+    idx: 0-MxN index of grid
+    grid: MxN array
+    """
+    i = math.floor(idx / grid.shape[0])
+    j = idx % grid.shape[1]
+    return int(i), int(j)
+def next_state_from_action(state, a, grid):
+    i, j = state_idx_to_ij(state, grid)
+    m, n = grid.shape
+    if a == 0 and j != n-1:
+       j += 1
+    elif a == 1 and i != 0:
+        i -= 1
+    elif a == 2 and j != 0:
+        j -= 1
+    elif a == 3 and i != m-1:
+        i += 1
+    return (i * n) + j
+def create_transitions(grid):
+    """
+    grid: MxN grid of color indices
+    """
+    M = grid.shape[0]
+    N = grid.shape[1]
+    N_STATES = M * N
+    N_ACTIONS = 4
+    # transitions[s, s1, a] = prob of landing in s1 when taking action a from s
+    transitions = np.zeros((N_STATES, N_STATES, N_ACTIONS))
+
+    for s in range(N_STATES):
+        for s1 in range(N_STATES):
+            for a in range(N_ACTIONS):
+                i, j = state_idx_to_ij(s, grid)
+                i1, j1 = state_idx_to_ij(s1, grid)
+                if a == 0:
+                    # going right, not at right edge
+                    if i == i1 and j < grid.shape[1] - 1 and j1 == j + 1:
+                        transitions[s, s1, a] = 1
+                elif a == 1:
+                    # going up, not at top
+                    if j == j1 and i > 0 and i1 == i - 1:
+                        transitions[s, s1, a] = 1
+                elif a == 2:
+                    # going left, not at left edge
+                    if i == i1 and j > 0 and j1 == j -1:
+                        transitions[s, s1, a] = 1
+                elif a == 3:
+                    # going down, not at bottom
+                    if j == j1 and i < grid.shape[0] - 1 and i1 == i + 1:
+                        transitions[s, s1, a] = 1
+    return transitions
+def featurize(grid, feature_idxs):
+    """
+    grid: 10x10 color grid, colors are -1, -0.5, 0, 0.5, 1
+    feature_idxs: dictionary, maps colors to feature index 
+        {-1:0, -0.5:1,...}
+    returns list of featurized states
+    """
+    feature_map = []
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            color = grid[i, j]
+            features = np.zeros(len(feature_idxs))
+            if color in feature_idxs.keys():
+                features[feature_idxs[color]] += 1
+            feature_map.append(features)
+    return np.array(feature_map)
+def value_iteration(transitions, rewards, goal, gamma, threshold):
+    """
+    transition: NxNxN_ACTIONS - transition dynamics
+    rewards: Nx1 - state rewards
+    goal: terminal state in MDP, set policy here as -1: no action
+    gamma: discount factor
+    threshold: threshold for stopping
+    returns optimal policy
+    """
+    N, _, N_ACTIONS = transitions.shape
+    values = np.zeros((N))
+
+    finished = False
+    while not finished:
+        values_copy = values.copy()
+        for s in range(N):
+            values[s] = max([sum([transitions[s, s1, a]*(rewards[s] + gamma*values_copy[s1]) 
+                                for s1 in range(N)]) 
+                                for a in range(N_ACTIONS)])
+        if (np.abs(values - values_copy) < threshold).all():
+            finished = True
+    policy = np.zeros([N])
+    for s in range(N):
+        policy[s] = np.argmax([sum([transitions[s, s1, a]*(rewards[s] + gamma*values[s1])
+                               for s1 in range(N)])
+                               for a in range(N_ACTIONS)])
+    policy[goal] = -1 # goal is terminal state, don't take more actions from here
+    return policy
+def feature_counts(policy, feature_map, state, action=None, gamma=1):
+    """
+    policy: Nx1 - mapping of state to optimal action
+    feature_map: NxN_FEATURES - list of features per state
+    state: start state
+    action: action to take from state 
+
+    returns: feature counts from taking given action from given state
+             to the goal state
+    """
+    N, N_FEATURES = feature_map.shape
+
+    if action is None:
+        action = policy[state]
+
+    feature_counts = np.zeros(N_FEATURES)
+    grid_size = (int(np.sqrt(N)), int(np.sqrt(N)))
+    dummy_grid = np.zeros(grid_size)
+    # while not at terminal state
+    i = 0
+    while policy[state] != -1:
+        feature_counts += np.power(gamma, i) * feature_map[state]
+        next_state = int(next_state_from_action(state, action, dummy_grid))
+        state = next_state
+        action = int(policy[state])
+        i += 1
+    return feature_counts
